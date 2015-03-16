f481064a05efbc69015a54179af5385f2973ace2
Anna <waterpolocity@gmail.com>
2015-03-16 00:36:13 -0700

    deleted files

--- Roomie/src/databaseConnection/DBCredentials.java
+++
@@ -1,8 +0,0 @@
-package databaseConnection;
-
-class DBCredentials {	
-	public static final String MYSQL_USERNAME = "ccs108cserepy";
-	public static final String MYSQL_PASSWORD = "eetiefoh";
-	public static final String MYSQL_DATABASE_SERVER = "mysql-user.stanford.edu";
-	public static final String MYSQL_DATABASE_NAME = "c_cs108_cserepy";
-}
